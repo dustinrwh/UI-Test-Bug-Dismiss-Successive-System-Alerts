bfc179480b515d3101a5e88c8edbc0bb50c3c946
Dustin Hill <dustin.hill@gm.com>
2016-02-11 12:01:04 -0600

    Initial Commit

---
+++ UI Test Dismiss System Alerts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UI Test Dismiss System Alerts
+//
+//  Created by Dustin Hill on 2/11/16.
+//  Copyright © 2016 Dustin Hill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UI Test Dismiss System Alerts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UI Test Dismiss System Alerts
+//
+//  Created by Dustin Hill on 2/11/16.
+//  Copyright © 2016 Dustin Hill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
